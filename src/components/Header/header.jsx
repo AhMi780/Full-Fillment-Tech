c541f43263b4735fd0cb7aa27a77f545c682599d
Ahmad <mohammadahmad441@gamil.com>
2021-04-05 10:55:04 +0500

    new project in antd

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,90 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { ArrowRightOutlined } from "@ant-design/icons";
+
+export default function Header() {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slideToShow: 1,
+    slideToScroll: 1,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          arrows: false,
+          dots: true,
+        },
+      },
+    ],
+  };
+  return (
+    <Slider {...settings} className="slider">
+      <div className="slider_card1 ps-relative index-1 overlay">
+        <div className="container contaner-md container-sm pl-15 pr-15 bs-bb">
+          <div className="slide_content_div">
+            <h2 className="slide-title">
+              We Run All Kinds Of IT Services That Vow Your Success
+            </h2>
+            <p className="slide-desc">
+              We are experienced professionals who understand that It services
+              is changing, and are true partners who care about your success.
+              Our team provides a consultative approach on emerging technology.
+            </p>
+            <div className="d-flex flex-wrap">
+              <a
+                href="#"
+                className="btn_header btn ps-relative index-1 no-underline bg-green d-flex justify-center item-center text-white mr-30 mb-10"
+              >
+                More About Us
+                <i className="icon-arrow-right d-flex justify-center item-center ml-10">
+                  <ArrowRightOutlined />
+                </i>
+              </a>
+              <a
+                href="#"
+                className="btn_header btn ps-relative index-1 no-underline bg-white d-flex justify-center item-center text-black"
+              >
+                Our Services
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="slider_card2 overlay ps-relative index-1">
+        <div className="container contaner-md container-sm pl-15 pr-15 bs-bb">
+          <div className="slide_content_div">
+            <h2 className="slide-title">
+              Keep Business Safe And Ensure High Availability.{" "}
+            </h2>
+            <p className="slide-desc">
+              We are experienced professionals who understand that It services
+              is changing, and are true partners who care about your success.
+              Our team provides a consultative approach on emerging technology.
+            </p>
+            <div className="d-flex flex-wrap">
+              <a
+                href="#"
+                className="btn_header btn ps-relative index-1 no-underline bg-green d-flex justify-center item-center text-white mr-30 mb-10"
+              >
+                More About Us
+                <i className="icon-arrow-right d-flex justify-center item-center ml-10">
+                  <ArrowRightOutlined />
+                </i>
+              </a>
+              <a
+                href="#"
+                className="btn_header btn ps-relative index-1 no-underline bg-white d-flex justify-center item-center text-black"
+              >
+                Our Services
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Slider>
+  );
+}
---
+++ src/components/navBar/nav.jsx
@@ -0,0 +1,91 @@
+import BrandLight from "../../assets/images/logo/logo-light.png";
+import BrandDark from "../../assets/images/logo/logo-dark.png";
+import React, { useState, useEffect } from "react";
+import { HiUser } from "react-icons/hi";
+import { Link, Switch } from "react-router-dom";
+
+export default function NavBar() {
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 50);
+    });
+  }, []);
+  return (
+    <Switch>
+      <nav className={scroll ? "sticky_nav" : "header_nav w-100 d-flex "}>
+        <div className="container d-flex item-center justify-between pr-15 pl-15 h-100 bs-bb">
+          <Link to="/" className="nav_brand flex-1">
+            <img
+              className=" light-sticky d-none-md"
+              src={BrandLight}
+              alt="brand.com"
+            />
+            <img
+              className="dark-sticky d-none d-block-md "
+              src={BrandDark}
+              alt="brand.com"
+            />
+          </Link>
+          <div className="main_navigation d-flex pl-15 pr-15 bs-bb h-100 flex-3 flex-1-sm justify-content-end ">
+            <ul className="header_ul d-flex d-none-md h-100 ">
+              <li className="li_home li-style-none ps-relative">
+                <Link to="/" className="no-underline text-white">
+                  Home
+                </Link>
+              </li>
+              <li className="li_company li-style-none">
+                <Link to="/about" className="no-underline text-white">
+                  Company
+                </Link>
+              </li>
+              <li className="li_it_soloution li-style-none">
+                <a href="" className="no-underline text-white">
+                  {" "}
+                  IT Solutions
+                </a>
+              </li>
+              <li className="li_news li-style-none">
+                <a href="" className="no-underline text-white">
+                  {" "}
+                  News&Media
+                </a>
+              </li>
+              <li className="li-style-none">
+                <a href="" className="no-underline text-white">
+                  {" "}
+                  Features
+                </a>
+              </li>
+              <li className="li-style-none mr-0">
+                <a href="" className="no-underline text-white">
+                  {" "}
+                  Contacts
+                </a>
+              </li>
+            </ul>
+
+            <ul className="nav_action_buttons li-style-none d-flex item-center">
+              <li className="ml-20 nav_action_btn_quote d-none-lg">
+                <a
+                  href=""
+                  className="nav_quote_btn btn no-underline bg-white text-black d-flex justify-center item-center"
+                >
+                  Request A Quote
+                </a>
+              </li>
+              <li className="ml-20 ">
+                <button className="login_btn bg-trans text-white-lg ">
+                  <i className="user_icon text-blue text-white-lg">
+                    <HiUser />
+                  </i>
+                  <span>Login</span>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </Switch>
+  );
+}
